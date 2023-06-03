15675eb63a5ede2947576dc51e55bd4ab93b9b6a
Allen Ning <npc1222@icloud.com>
2023-06-04 00:35:33 +1000

    fixed bug to picking up the right format of repo urls

--- clone_repositories.go
+++ clone_repositories.go
@@ -3,18 +3,41 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/url"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 	"sync"
 )
 
-func cloneRepositories(config *Config, downloadDir string) {
+func cloneRepositories(config *Config, downloadDir string) error {
 	var wg sync.WaitGroup
 
+	// Check if download directory exists, create it if it doesn't
+	if _, err := os.Stat(downloadDir); os.IsNotExist(err) {
+		err := os.MkdirAll(downloadDir, 0755)
+		if err != nil {
+			return fmt.Errorf("failed to create download directory: %v", err)
+		}
+	}
+
 	for _, repo := range config.Repositories {
+		// Parse the repo URL into a URL type
+		url, err := url.Parse(repo)
+		if err != nil {
+			return fmt.Errorf("failed to parse repo URL: %v", err)
+		}
+
+		// skip existing repo
+		if isWithin(url, downloadDir) {
+			continue
+		}
+
 		wg.Add(1)
 		go func(repo string) {
 			defer wg.Done()
-			err := cloneRepo(config.Token, repo, downloadDir)
+			err := cloneRepo(url, downloadDir, config.Token)
 			if err != nil {
 				log.Printf("Failed to clone repository %s: %v\n", repo, err)
 			}
@@ -22,12 +45,17 @@ func cloneRepositories(config *Config, downloadDir string) {
 	}
 
 	wg.Wait()
+
+	return nil
 }
 
-func cloneRepo(repo, downloadDir, token string) error {
-	fmt.Printf("Cloning repository: %s\n", repo)
+func cloneRepo(u *url.URL, downloadDir, token string) error {
+	fmt.Printf("Cloning repository: %s\n", u)
 
-	cmd := exec.Command("git", "clone", fmt.Sprintf("https://%s:x-oauth-basic@github.com/%s", token, repo))
+	// Construct the new URL with the token and modified repo path
+	newRepo := fmt.Sprintf("%s://%s:x-oauth-basic@%s", u.Scheme, token, u.Host+u.Path)
+
+	cmd := exec.Command("git", "clone", newRepo)
 	cmd.Dir = downloadDir
 	output, err := cmd.CombinedOutput()
 
@@ -35,6 +63,11 @@ func cloneRepo(repo, downloadDir, token string) error {
 		return fmt.Errorf("clone error: %v, output: %s", err, string(output))
 	}
 
-	fmt.Printf("Repository cloned: %s\n", repo)
+	fmt.Printf("Repository cloned: %s\n", u)
 	return nil
+}
+
+func isWithin(u *url.URL, downloadDir string) bool {
+	repoPath := filepath.Join(downloadDir, filepath.Base(u.Path))
+	return strings.HasPrefix(repoPath, downloadDir)
 }
